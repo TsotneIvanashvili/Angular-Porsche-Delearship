d09ebecd9d2a779d72146f70481420dddd686e2b
TsotneIvanashvili <Tsotneivanashvili@gmail.com>
2025-08-02 12:41:02 +0400

    main

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { CarsComponent } from './cars/cars';
 import { DetailsComponent } from './details/details';
+import { Home } from './home/home';
 
 export const routes: Routes = [
-  { path: '', component: CarsComponent },
+  { path: '', component: Home },
   { path: 'details/:id', component: DetailsComponent },
 ];
